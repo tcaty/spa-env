05534bc461e8daf57371c4a4ff4304a54db7e24c
Maksim Kamanin <ltcatyl@gmail.com>
2024-05-07 20:55:32 +0300

    logger created

---
+++ internal/logger/discradwriter.go
@@ -0,0 +1,12 @@
+package logger
+
+type discardWriter struct {
+}
+
+func newDiscardWriter() *discardWriter {
+	return &discardWriter{}
+}
+
+func (dw *discardWriter) Write(p []byte) (n int, err error) {
+	return 0, nil
+}
